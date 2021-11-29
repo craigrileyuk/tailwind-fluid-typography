912934c7f8518dcdccfc6566d0539179c02485cf
Craig Riley <github@craigriley.uk>
2021-11-29 02:00:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+<img alt="Tailwind Fluid Typography" width="100" src="./.github/logo.png">
+
+# Tailwind Fluid Typography
+
+Based on the fluid typography theory devised by [Mike Riethmuller](https://madebymike.com.au) and incorporating ideas from Google's Material Design spec, Tailwind Fluid Typography gives you a new set of utility classes that scale modularly depending on screen size.
+
+- Set breakpoints for when your type should start and stop scaling
+- Dual scaling system gives you the ability to set your desired scale for your lower breakpoint and for your higher breakpoint
+- Provides base, lg, xl, 2xl, 3xl, 4xl, 5xl, 6xl, 7xl, 8xl and 9xl font-sizes
+
+## Installation
+
+```
+npm i tailwind-fluid-typography
+// or
+yarn add tailwind-fluid-typography
+```
+
+```js
+// tailwind.config.js
+module.exports = {
+    theme: {
+        fluidTypography: {}
+    },
+    plugins: [
+        require('tailwind-fluid-typography')
+    ]
+}
+```
+
+## Usage
+```html
+<h1 class="fluid-4xl">Fluid Typography @ 4XL</h1>
+<h2 class="fluid-3xl">Fluid Typography @ 3XL</h2>
+<h3 class="fluid-2xl">Fluid Typography @ 2XL</h3>
+<h4 class="fluid-xl">Fluid Typography @ XL</h4>
+<h5 class="fluid-lg">Fluid Typography @ LG</h5>
+<p class="fluid-base">Fluid Typography</p>
+```
+
+## Customisation
+
+To customise the plugin settings, you can pass the following properties as part of a `fluidTypography` property on `theme`:
+
+| Name          | Type   | Default | Description |
+| ------------- | ------ | ------- | ----------- |
+| remSize       | Number | 16      | The px size to assume for 1rem |
+| minScreenSize | Number | 320     | The screen size (in px) at which to begin scaling |
+| maxScreenSize | Number | 1920    | The screen size (in px) at which to stop scaling |
+| minTypeScale  | Number | 1.2     | The scaling factor to use at minScreenSize |
+| maxTypeScale  | Number | 1.333   | The scaling factor to use at maxScreenSize |
+| lineHeight    | Number | 1.35    | The line-height to use for heading classes |
+
+For example:
+
+```js
+theme: {
+    fluidTypography: {
+        remSize: 14,
+        minScreenSize: 600,
+        maxScreenSize: 1280,
+        minTypeScale: 1.250
+        maxTypeScale: 1.618
+        lineHeight: 1.5
+    }
+}
+```
+
+### Suggested type scales
+
+|                  |       |                      |
+| ---------------- | ----- | -------------------- |
+| Minor Second     | 1.067 |                      |
+| Major Second     | 1.125 |                      |
+| Minor Third      | 1.200 | default minTypeScale |
+| Major Third      | 1.250 |                      |
+| Perfect Fourth   | 1.333 | default maxTypeScale |
+| Augmented Fourth | 1.414 |                      |
+| Perfect Fifth    | 1.500 |                      |
+| Golden Ratio     | 1.618 |                      |
---
+++ index.js
@@ -0,0 +1,119 @@
+const plugin = require("tailwindcss/plugin");
+
+// React Demo: https://codesandbox.io/s/clamp-linear-intepolation-based-on-viewport-width-builder-xgkft
+
+function generateFluidRule(power, { minTypeScale, maxTypeScale, minScreenSize, maxScreenSize, remSize }) {
+	const minFontSize = Math.pow(minTypeScale, power);
+	const maxFontSize = Math.pow(maxTypeScale, power);
+	// Transform the screen sizes from px values to expected rem values
+	const minScreenSizeRem = minScreenSize / remSize;
+	const maxScreenSizeRem = maxScreenSize / remSize;
+	// Quick function to round calculations to a locked number of decimal places
+	const r = (v) => v.toFixed(4);
+
+	const slope = (maxFontSize - minFontSize) / (maxScreenSizeRem - minScreenSizeRem);
+	let interpolation = minFontSize - slope * minScreenSizeRem;
+	let sign = "+";
+	if (interpolation < 0) {
+		sign = "-";
+		interpolation = Math.abs(interpolation);
+	}
+
+	// Return the formatted CSS value for the font-size
+	return `clamp(${r(minFontSize)}rem, calc(${r(slope * 100)}vw ${sign} ${r(interpolation)}rem), ${r(
+		maxFontSize
+	)}rem)`;
+}
+
+const fluidTypography = function ({ addUtilities, theme }) {
+	const fluidConfig = {};
+	/**
+	 * Suggested px value of rem. Since all browsers use 16, this is the default
+	 */
+	fluidConfig.remSize = theme("fluidTypography.remSize", 16);
+	/**
+	 * This controls the scale at which the font sizes will increase at the lowest bounds (e.g. on mobile devices)
+	 */
+	fluidConfig.minTypeScale = theme("fluidTypography.minTypeScale", 1.2);
+	/**
+	 * This controls the scale at which the font sizes will increase at the highest bounds (e.g. on desktop computers)
+	 */
+	fluidConfig.maxTypeScale = theme("fluidTypography.maxTypeScale", 1.333);
+	/**
+	 * The px size of the screen at which you want the font-size to stop decreasing
+	 */
+	fluidConfig.minScreenSize = theme("fluidTypography.minScreenSize", 320);
+	/**
+	 * The px size of the screen at which you want the font-size to stop increasing
+	 */
+	fluidConfig.maxScreenSize = theme("fluidTypography.maxScreenSize", 1920);
+	/**
+	 * The relative line-height of the headings
+	 */
+	fluidConfig.lineHeight = theme("fluidTypography.lineHeight", 1.35);
+
+	// letterSpacing sizes based on https://vuetifyjs.com/en/styles/text-and-typography/#typography
+	const rules = {
+		".fluid-base": {
+			fontSize: "1rem",
+			lineHeight: 1.4,
+			letterSpacing: 0.5,
+		},
+		".fluid-lg": {
+			fontSize: generateFluidRule(1, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "0.009375em",
+		},
+		".fluid-xl": {
+			fontSize: generateFluidRule(2, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "0.0125em",
+		},
+		".fluid-2xl": {
+			fontSize: generateFluidRule(3, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "normal",
+		},
+
+		".fluid-3xl": {
+			fontSize: generateFluidRule(4, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "0.0073529412em",
+		},
+
+		".fluid-4xl": {
+			fontSize: generateFluidRule(5, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "normal",
+		},
+		".fluid-5xl": {
+			fontSize: generateFluidRule(6, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "-0.0083333333em",
+		},
+		".fluid-6xl": {
+			fontSize: generateFluidRule(7, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "-0.015625em",
+		},
+		".fluid-7xl": {
+			fontSize: generateFluidRule(8, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "-0.0234375em",
+		},
+		".fluid-8xl": {
+			fontSize: generateFluidRule(9, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "-0.03125em",
+		},
+		".fluid-9xl": {
+			fontSize: generateFluidRule(10, fluidConfig),
+			lineHeight: fluidConfig.lineHeight,
+			letterSpacing: "-0.04em",
+		},
+	};
+
+	addUtilities(rules);
+};
+
+module.exports = plugin(fluidTypography);
